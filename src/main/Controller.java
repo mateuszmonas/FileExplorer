37d2437834ba8ffe2268fbc20c6be1dbe1b6ab75
mateuszmonas <mateuszmonas@gmail.com>
2018-05-16 16:50:47 +0200

    commit

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -2,28 +2,31 @@ package main;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
 
     private View view;
-    private String path;
+    private String[] path = new String[2];
 
     Controller(View view) {
-        path="D:";
+        path[0] = "C:";
+        path[1] = "D:";
         this.view = view;
     }
 
     void start(){
-        getFiles();
+        getFiles(0);
+        getFiles(1);
     }
 
     /**
      * Gets all files in the given path
      * and updates the view
      */
-    void getFiles(){
-        File folder = new File(path);
+    void getFiles(int whichList){
+        File folder = new File(path[whichList]);
         File[] fileList = folder.listFiles();
-        view.displayFiles(fileList);
+        view.displayFiles(fileList, whichList);
     }
 }
--- src/main/View.java
+++ src/main/View.java
@@ -1,47 +1,40 @@
 package main;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class View implements Initializable {
 
     private Stage primaryStage;
     private Controller controller;
-    @FXML private VBox cur_files;
-    @FXML VBox menu;
-    @FXML Button a;
-
-    @FXML protected void test(ActionEvent event) {
-        System.out.println("lollolol");
-    }
+    @FXML private VBox filesA;
+    @FXML private VBox filesB;
+    @FXML private TextField filePathA;
+    @FXML private TextField filePathB;
+    private TextField[] filePaths = new TextField[2];
+    private VBox[] fileLists= new VBox[2];
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         controller = new Controller(this);
+        fileLists[0] = filesA;
+        fileLists[1] = filesB;
+        filePaths[0]= filePathA;
+        filePaths[1]= filePathB;
         controller.start();
     }
 
-    private void changeDirectory(String path){
+    private void changeDirectory(String path, int whichList){
     }
 
     private void editFiles(File[] files){
@@ -49,19 +42,19 @@ public class View implements Initializable {
             TextField textField = new TextField();
             textField.setText(file.getName());
             textField.setStyle("-fx-padding: 0 0 0 0;");
-            cur_files.getChildren().add(textField);
+            filesA.getChildren().add(textField);
         }
     }
 
 
-    private void viewFiles(File[] files){
+    private void viewFiles(File[] files, int whichList){
         for (File file : files) {
             Text label = new Text();
             label.setText(file.getName());
             label.setOnMouseClicked(event -> {
-                if (file.isDirectory()) changeDirectory(file.getPath());
+                if (file.isDirectory()) changeDirectory(file.getPath(), whichList);
             });
-            cur_files.getChildren().add(label);
+            fileLists[whichList].getChildren().add(label);
         }
     }
 
@@ -69,13 +62,15 @@ public class View implements Initializable {
      * displays the names of all the files in the given path
      * @param files array of files to display
      */
-    void displayFiles(File[] files){
+    void displayFiles(File[] files, int whichList){
+        fileLists[whichList].getChildren().clear();
         if(files!=null) {
-            viewFiles(files);
+            filePaths[whichList].setText(files[0].getParent());
+            viewFiles(files, whichList);
         } else {
             Label label = new Label();
             label.setText("directory does not exist");
-            cur_files.getChildren().add(label);
+            filesA.getChildren().add(label);
         }
     }
 
