0be79fef998e9817fe3de5617473556060ed439f
mateuszmonas <mateuszmonas@gmail.com>
2018-05-19 16:04:24 +0200

    commit

--- src/file/FileTransferable.java
+++ src/file/FileTransferable.java
@@ -7,6 +7,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * wrapper class required for copying files to teh clipboard
+ */
 public class FileTransferable implements Transferable {
 
     private List<File> listOfFiles;
